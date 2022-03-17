e2f8ac34887a6a464797c7cfa6ad660090761f6e
AkiraHonda <05410kira@gmail.com>
2022-03-17 15:00:05 +0900

    useRef,non-null assertion operator

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -23,13 +23,21 @@ const Counter: React.FC<{}> = () => {
   useEffect(() => {
     renderTimes.current = renderTimes.current + 1;
   });
-  
+  // non-null assertion operator
+  const ref = useRef<HTMLInputElement>(null!);
+  const focusInput = () => {
+    const current = ref.current;
+    if (current != null) current.focus();
+  };
+
   return (
     <div>
       <div>value: {value}</div>
       <button onClick={increment}>+1</button>
       <button onClick={decrement}>-1</button>
       <div>This component was re-rendered {renderTimes.current} times!</div>
+      <input ref={ref} type="text" />
+      <button onClick={focusInput}>Click Me!</button>
     </div>
   );
 };
